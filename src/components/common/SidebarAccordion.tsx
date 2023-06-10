bd0cbbb00e92322990e07d3cd0cabffda6b73f29
faria-karim-porna <faria.porna.08@gmail.com>
2023-06-10 09:46:36 +0000

    Partially Fixed Sidebar Accordion

--- src/components/common/SidebarAccordion.tsx
+++ src/components/common/SidebarAccordion.tsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 type SidebarAccordionProps = {
   view?: string;
   outer?: string;
   inner?: SidebarAccordionProps[] | [];
+  path?: string;
 };
 
 const SidebarAccordionComponent = (props: SidebarAccordionProps) => {
-  const { view, inner, outer } = props;
+  const { view, outer, inner } = props;
+
+  const [viewInner, setViewInner] = useState<string[]>([]);
   return (
     <>
-      <div className="sidebar-accordion-outer d-flex justify-content-between">
+      <div className="sidebar-accordion-outer d-flex justify-content-between" onClick={() => setViewInner([...viewInner, view ?? ""])}>
         <div>{outer}</div>
         <div>caret</div>
       </div>
-      <div className="sidebar-accordion-inner">
+      <div className={`sidebar-accordion-inner ${viewInner.includes(view ?? "") ? "d-block" : "d-none"}`}>
         {inner?.map((accordionInner) =>
           (accordionInner.inner?.length ?? 0) > 0 ? (
             <SidebarAccordion view={accordionInner.view} outer={accordionInner.outer} inner={accordionInner.inner} />
