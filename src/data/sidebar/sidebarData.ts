69c259e869ea160285cd14c9a8e01f2b2daf4559
faria-karim-porna <faria.porna.08@gmail.com>
2023-06-10 07:44:52 +0000

    Added Sidebar Options

--- src/data/sidebar/sidebarData.ts
+++ src/data/sidebar/sidebarData.ts
@@ -15,3 +15,74 @@ const office = {};
 export const sidebarData = () => {
   return [cheatsheets];
 };
+
+
+// cheatsheets =>
+//// frontend => =>
+////// html
+////// css
+////// bootstrap
+////// react.js
+////// angular.js
+//// backend => =>
+////// node.js
+////// django
+////// flask
+////// laravel
+////// .NET
+//// programming languages => =>
+////// javascript 
+////// typescript
+////// c++
+////// c
+////// php
+////// python
+////// c#
+//// artificial intelligence => =>
+////// matplotlib
+////// keras
+////// tensorflow
+////// opencv
+////// theory
+//// game development => =>
+////// javascript
+////// unity
+//// blockchain
+//// ethical hacking
+//// app development => =>
+////// react native
+////// ionic
+////// flutter
+//// desktop app development => =>
+////// electron.js
+//// others => =>
+////// design patterns
+////// oops
+////// dbms
+////// computer networking
+////// computer architecture
+////// regex
+
+// data structure and algorithms
+
+// roadmaps
+
+// office learning =>
+//// p1ston
+//// p1ston widget
+//// regAnalytics
+
+// motivation
+
+// routine
+
+// current project
+
+// project list
+// => table (project type, name, stack, link)
+
+// other =>
+//// vs code extension
+//// chrome extension
+
+// todo
