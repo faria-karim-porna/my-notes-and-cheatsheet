d3e4e8012387f9a17cca5c666ebbe6305776f0e5
faria-karim-porna <faria.porna.08@gmail.com>
2023-06-19 21:05:33 +0000

    Cheatsheet Frontend Reactjs Page Added

---
+++ src/components/pages/CheatsheetFrontendReactjs.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Accordion } from "../common/Accordion";
+import { CodeBlock } from "../common/Codeblock";
+
+const CheatSheetFrontendReactJsComponent = () => {
+  return <>Welcome to react.js</>;
+};
+
+export const CheatSheetFrontendReactJs = React.memo(CheatSheetFrontendReactJsComponent);
