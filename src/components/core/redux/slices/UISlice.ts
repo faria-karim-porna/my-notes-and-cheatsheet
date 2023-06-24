d8e51c3ee061908af99e33c035c43d79ab1dc217
faria-karim-porna <faria.porna.08@gmail.com>
2023-06-24 07:23:39 +0000

    Added Sidebar Button

--- src/components/core/redux/slices/UISlice.ts
+++ src/components/core/redux/slices/UISlice.ts
@@ -1,15 +1,21 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface UIState {
+  showSidebar: boolean;
   pageView?: string;
 }
 
-const initialState: UIState = {};
+const initialState: UIState = {
+  showSidebar: true,
+};
 
 const UISlice = createSlice({
   initialState,
   name: "sliceUI",
   reducers: {
+    setSidebarView(state, action: PayloadAction<boolean>) {
+      state.showSidebar = action.payload;
+    },
     setPage(state, action: PayloadAction<string>) {
       state.pageView = action.payload;
     },
