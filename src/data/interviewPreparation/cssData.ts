3bcf46e4a94a8777fb43aafd537a79408021997c
faria-karim-porna <faria.porna.08@gmail.com>
2023-07-06 07:07:01 +0000

    Updated Structure For CSS Page Of Interview Preparation

---
+++ src/data/interviewPreparation/cssData.ts
@@ -0,0 +1,58 @@
+export const cssData = [
+  {
+    questions: `What is CSS?`,
+    answers: `CSS stands for Cascading Style Sheet. It is a popular styling language. It is used with HTML to design websites. It can also be used with any XML documents including plain XML, SVG, and XUL.`,
+  },
+
+  {
+    questions: `What is the origin of CSS?`,
+    answers: `SGML (Standard Generalized Markup Language) is the origin of CSS. It is a language that defines markup languages.`,
+  },
+
+  {
+    questions: `What are the different variations of CSS?`,
+    answers: `The different variations of CSS are CSS1, CSS2, CSS2.1, CSS3, CSS4.`,
+  },
+
+  {
+    questions: `How can you integrate CSS on a web page?`,
+    answers: `CSS can be integrated into a web page in three ways: inline, internal, and external. Inline CSS is directly added within the HTML tags using the "style" attribute. Internal CSS is placed within the "style" tags in the head section of an HTML document. External CSS is stored in a separate CSS file and linked to the HTML document using the "link" tag.`,
+  },
+
+  {
+    questions: `What are the advantages of CSS?`,
+    answers: `The advantages of CSS include separation of style from content, which makes maintenance and updates easier; consistent styling across multiple web pages; improved page loading speed due to reduced HTML code; the ability to create responsive designs for different devices and screen sizes; and a wide range of design options and flexibility.`,
+  },
+
+  {
+    questions: `What are the limitations of CSS?`,
+    answers: `Limitations of CSS include browser compatibility issues, as different browsers may interpret CSS code differently; CSS is primarily a styling language and has limited capabilities for interactivity and dynamic behavior. More complex interactions often require JavaScript or other scripting languages. Css provides basic mathematical operators for values like width and height, but it lacks the ability to perform more advanced calculations without the use of CSS preprocessors or JavaScript.`,
+  },
+
+  {
+    questions: `What are the CSS frameworks?`,
+    answers: `They offer ready-to-use CSS styles, layout grids, and components, allowing developers to streamline the development process and create responsive and consistent designs more efficiently. Examples of popular CSS frameworks include Bootstrap, Foundation, and Bulma. Bootstrap, Foundation, Semantic UI, Gumby, Ulkit,`,
+  },
+
+  {
+    questions: `Why background and color are the separate properties if they should always be set together?`,
+    answers: `Background and color are separate properties in CSS because they serve different purposes. The background property defines the background color or image of an element, while the color property defines the text color.`,
+  },
+
+  {
+    questions: `What is Embedded Style Sheet?`,
+    answers: `An Embedded Style Sheet is CSS code that is placed within the HTML document itself, typically within the "style" tags in the head section.For example: <style>    
+            body {    
+                background-color: linen;    
+            }    
+            h1 {    
+                color: red;    
+                margin-left: 80px;    
+            }     
+            </style> `,
+  },
+  {
+    questions: `What are the advantages of Embedded Style Sheets?`,
+    answers: `The advantages of Embedded Style Sheets simplifying maintenance as the styles are contained within the document, and No extra download is required to import the information.`,
+  },
+];
