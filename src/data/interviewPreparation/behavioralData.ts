fe3b1a35fa130fc9f7525560a8e764c8793d7d94
faria-karim-porna <faria.porna.08@gmail.com>
2023-07-10 10:40:10 +0000

    Added About Me In The Behavioral Interview Questions

--- src/data/interviewPreparation/behavioralData.ts
+++ src/data/interviewPreparation/behavioralData.ts
@@ -1,6 +1,21 @@
 export const behavioralData = [
   {
-    questions: "",
-    answers: "",
+    questions: `Tell us something about yourself which is not mentioned in your cv.`,
+    answers: `Yeah sure. Thank you for the opportunity to share more about myself. I am Faria. Currently, I am working as a Software Engineer at Kaz Software where I am involved in testing and debugging code, implementing best coding practices, and ensuring the software is scalable and efficient. 
+
+    I have been working here since march of 2021. I have joined here as an associate software engineer. Recently I got promoted. 
+    In my current company, I have mostly been involved in front-end development for a significant portion of the time. In the span of 2 years, I have been involved in implementing many new features and also have been part of the significant enhancements of the existing features using react.js, redux and typescript. Additionally, to date now I have done code reviews for the backend part of the application and also have worked on performance improvements, and have been involved in optimizing queries.
+    
+    Although I have worked relatively less in the backend field in my career so far, but I have used many backend technologies for the learning purpose in many of my personal projects. In those projects, my main focus was on performing CRUD operations and implementing robust authentication and authorization systems.
+    
+    Although I have a basic understanding of various backend technologies, I am mostly familiar with Node.js.
+    
+    I have completed my graduation from North South University in the year 2020. I have achieved the highest “Summa Cum Laude” prestige from my university for having an excellent CGPA.
+    
+    In my free time I love to watch indian and south indian movies. Movies which are based on student lives are my most favourite such as 3 idiots, chichore, vaathi, etc.
+    
+    My hobby is to do illustration and animation kind of stuff using powerpoint. I just love this tool. Every creative and artistic kind of things always fascinates me.
+    
+    Yeah, that's pretty much about me.`,
   },
 ];
