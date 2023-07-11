1473eace56c65613b128afc8abcf3f4f1ee593e3
faria-karim-porna <faria.porna.08@gmail.com>
2023-07-11 11:29:30 +0000

    Added Total 17 Questions To The Behavioral Questions Page Of Interview Preparation

--- src/data/interviewPreparation/behavioralData.ts
+++ src/data/interviewPreparation/behavioralData.ts
@@ -42,18 +42,64 @@ export const behavioralData = [
     questions: `Describe a time when you worked on a project as part of a team. How did you contribute to the team's success?`,
     answers: `In a team project, I contributed to the team's success by participating in collaborative discussions, sharing my ideas, and listening to others' perspectives, completing tasks that are assigned to me and also supporting teammates in areas where they needed assistance.`,
   },
+
+  {
+    questions: `Why did you choose Front-End Development as your career?`,
+    answers: `I love the creative and artistic aspect of designing functional user interfaces in front-end development. It's a rewarding and exciting career that blends my creativity, technical skills, and passion for problem-solving.`,
+  },
+
+  {
+    questions: `How many years you have been professionally working as a Javascript engineer?`,
+    answers: `I have been professionally working as a Javascript engineer for more than 2 years.`,
+  },
+
+  {
+    questions: `What are the challenges faced by the Front-end Development?`,
+    answers: `Front-end development challenges include cross-browser compatibility, responsive design, performance optimization, and staying up-to-date with evolving technologies.`,
+  },
+
+  {
+    questions: `Provide links to the projects that you worked on and felt proud of it.`,
+    answers: `I felt proud of my thesis project which is a machine learning-based project that aimed to predict uttered words based solely on lip movement without any sound. We hope to publish it. So for the privacy reason, I can not share the link of the project.`,
+  },
+
+  {
+    questions: `How would you respond if a colleague interrupted you while you were in the middle of an assignment?`,
+    answers: `If the interruption was urgent, I'd either address the task immediately or delegate it to another team member. Otherwise, I'd request my colleague to wait until I finish my current task, so I can provide my full attention for the best response.`,
+  },
+
+  {
+    questions: `What are your salary expectations?`,
+    answers: `I expect a competitive salary reflecting my experience and responsibilities. In my opinion, a competitive salary motivates employees. However, work culture and growth opportunities matter too. But if I've to mention the figure, then I'd say 1000 USD.`,
+  },
+
+  {
+    questions: `What are your Biggest Strengths and Weaknesses?`,
+    answers: `First, I want to start with my strengths. I am a responsible person. I always value other people's time and contribution. I am also a passionate person. I don’t stop until I am done. I am a curious person as well. I love to keep learning. On the other hand, if it comes to weaknesses, then I have to say that I tend to be a perfectionist. Sometimes I put a lot of effort on very small details. Sometimes I become impatient to complete the work very quickly. I am a self critic. I very often feel that it is possible to do far better work from my side. So I spend too much time reviewing and revising work.`,
+  },
+
+  {
+    questions: `What are your Long-Term Plans?`,
+    answers: `As a software engineer, I aim to build a successful career by expanding my expertise. I want to see myself as a technical lead who can help many people and make a major impact on society by contributing to complex and large-scale projects.`,
+  },
+
+  {
+    questions: `Can you recall an instance where you had to explain a technical concept to a non-technical person?`,
+    answers: `While explaining a technical concept to a non-technical person, I focused on simplifying the language and I also focused on using relatable examples. Visual aids, diagrams, or demonstrations were helpful tools to convey the concept effectively.`,
+  },
+
+  {
+    questions: `Tell me about a time when you faced a setback or failure in a project. How did you handle it?`,
+    answers: `In one of the features of my current project I encountered a failure in delivering the feature on time. When encountering a setback or failure in a project, I acknowledged the situation, took responsibility for my part, and analyzed the root causes to learn from the experience. I sought feedback from teammates or supervisors to gain different perspectives and identify areas for improvement. I then adjusted my approach, made necessary changes, and used the setback as a valuable learning opportunity.`,
+  },
+
+  {
+    questions: `Describe a situation where you had to juggle multiple priorities or tasks simultaneously.`,
+    answers: `In my current project, there was a situation where multiple tasks or tickets demanded my attention. To manage effectively, I prioritized tasks based on deadlines and importance,, and then organized my time accordingly.`,
+  },
 ];
 
 // ====
-
-// Can you recall an instance where you had to explain a technical concept to a non-technical person?
-// Answer: While explaining a technical concept to a non-technical person, I focused on simplifying the language and using relatable examples or analogies. I ensured that I understood their level of understanding and tailored my explanation accordingly. Visual aids, diagrams, or demonstrations were helpful tools to convey the concept effectively.
-
-// Tell me about a time when you faced a setback or failure in a project. How did you handle it?
-// Answer: When encountering a setback or failure in a project, I acknowledged the situation, took responsibility for my part, and analyzed the root causes to learn from the experience. I sought feedback from teammates or supervisors to gain different perspectives and identify areas for improvement. I then adjusted my approach, made necessary changes, and used the setback as a valuable learning opportunity.
-
-// Describe a situation where you had to juggle multiple priorities or tasks simultaneously.
-// Answer: In a previous role, I regularly faced situations where multiple tasks or priorities demanded my attention. To manage effectively, I prioritized tasks based on urgency and impact, organized my time using techniques like time-blocking or task management tools, and maintained open communication with stakeholders to manage expectations. Regularly reviewing and adjusting priorities ensured I stayed on track.
 
 // How do you handle constructive feedback or criticism from colleagues or supervisors?
 // Answer: I view constructive feedback as an opportunity for growth and improvement. I actively listen to the feedback, appreciate different perspectives, and avoid taking it personally. I ask clarifying questions to gain a deeper understanding and take the necessary steps to address the feedback, incorporating it into my work to enhance my skills and performance.
@@ -62,3 +108,107 @@ export const behavioralData = [
 // Answer: In a cross-functional project, I collaborated with a team member who had a contrasting working style. To ensure a productive collaboration, I actively listened, sought common ground, and respected their perspectives. By focusing on our shared goals and maintaining open and clear communication, we were able to leverage our diverse strengths and successfully deliver the project.
 
 // Remember, these answers are just examples, and you can personalize them with your own experiences and achievements. It's important to showcase your problem-solving skills, teamwork, adaptability, and effective communication throughout the interview.
+
+// ===
+// Q: Where do you live?
+// A: At present, I live in Basabo.
+// ==========================================
+// Q: Who is there in your family?
+// A: In my family, there is my mom and my elder brother.
+// ==========================================
+// Q: What are they doing?
+// A: My mom is a housewife and my brother is searching for a job. He is a marketing graduate. Earlier he was interested to do his own business. But my family wants to do him job. Now he is convinced to do job.
+// ==========================================
+// Q: Why did you apply here?
+// A: Because welldev has a reputation as a multinational company. I have heard a lot of praise about the culture here. Here hard work is always valued. The seniors and colleagues here are very cooperative. Different kinds of technologies are dealing here which is quite interesting.
+// ==========================================
+
+// Q: Why should we hire you?
+// A: Because I am self-motivated and a curious person. I like to learn new techs and tools. As welldev deals with different types of technologies and I have  worked in different technologies ,  so I have the flexibility to adopt myself with new technologies and challenge.  I guess we both will gel well. And overall my skills are aligned with the requirements. I was involved in competitive programming. I love to do problem solving. I have previous experience in rest api, react.js, oop. Mostly I am ready to take all the responsibilities. I am hopeful that I have the ability to prove myself as an important asset to the company.
+// ==========================================
+// Q: Do you have any plans for higher study?
+// A: I don’t have any plans because I like practical stuffs more than theoretical studies and researches. I think things can be learnt quickly by implementing them. Although because of my CGPA it may seem like I put focus on theoretical stuffs but it is not like that. Actually I understood what my teachers teaches really well. I think I should utilize the things that I am good at. That's why I got good score in the theoretical topics. Moreover I always did contribution on my academic projects. I never wanted to be the person who did nothing in the project but only made her present on the day of submission.
+// ==========================================
+// Q: What are the tools/frameworks you learned recently?
+// A: Recently I have learned typescript and redux based tools like redux toolkit, redux, redux thunk.
+// ==========================================
+// Q: What is your salary expectation?
+// A: In my perspective, a competitive salary works as a motivation for the employees. But if I have to mention the number, then based on my versatile skills, hard work and prior experience in the job field, I would say 50 thousand.
+// ==========================================
+// Q: What would your siblings say about you?
+// A: I think they would say I am a hard working and passionate person.
+// ==========================================
+// Q: What will be the three positive and negative things that your brother will tell about you if he were asked?
+// A: I think he would say about my positive sides that I am a hardworking, studious and honest person. And about my negative side he would say that sometimes I take a lot of pressure unnecessarily and I am very straight forward and emotional.
+// ==========================================
+// Q: How long have you been working?
+// A: I have been working for 6 months in my current company.
+// ==========================================
+// Q: What have you done so far?
+// A: In my current company I am working on one of the most large scale based project of our company which is mainly ERP based project. So far I have done manual testing, fixed the bugs on the code, done some sort of refactoring, created new components like drag and drop based file uploader, search  bar with dropdown options.
+// ==========================================
+// Q: Do you prefer to work in team or solo?
+// A: Actually both of the areas have different advantages. The advantage of teamwork is that many large scale projects can be completed in a relatively short amount of time due to work sharing. There is a scope of sharing knowledge with others in a team work. Because of involvement of many people, team members get guidance and help. It is possible to create something innovative by combining the strong skills of the team members. On the other hand, a person can do whatever he thinks is beneficial if he is doing everything all alone. He can takes decision quickly. No one has to take the responsibility of team management.
+// ==========================================
+// Q: What are the 3 positive sides of programming?
+// A: I think there is a scope of showing creativity in programming. It is possible to ease people’s life by developing innovative softwares. There is a scope of always learning even without getting bored.
+// ==========================================
+// Q: Which programming language do you like most?
+// A: Actually it depends on the type of projects. If the project is a web app or mobile app then I will prefer Javascript. Because it is possible to build interactive UI very easily using js. I think no other programming languages can beat js in this area. There are many efficient and helpful frontend frameworks which are written in js. On the otherhand for the data science based project, I will prefer python. Because there are many built in functions and libraries which are very helpful to implement data science based functionality. There are many machine learning frameworks in python like tensorflow, pytorch, scikit learn, keras. Another language I want to mention for game development and computer based app development is C#. This language is very popular for this two area.
+// ==========================================
+// Q: Tell us about your best project?
+// A: My best project was my thesis project. Durning the time of implementation of this project I have faced a lot of challenges. It was mainly a machine learning based project by which we were trying to predict the uttered word without any sound only based on lip movement. To build this project, our motive was to help the mute people.
+// ==========================================
+// Q: What kind of challenges do you have faced in this project?
+// A: The first challenge which I have faced was about dataset. Actually we were dealing with Bengali based audiovisual dataset. But unfortunately we have not found any related dataset. To solve this problem we have built our own dataset by using movie.py. The source of raw data was news presentations. Next challenge which we faced was about dataset representation and mapping output with input. As we were working on video dataset of variable length, so found it really very challenging. We have cutted each videos into frames. Then we grouped together all the frames which belong to the same video. After that we grouped videos based on their output labels. To make the number of frames equal for each video we used zero padding to our feature vector.
+// ==========================================
+// Q: What technologies have you used in your best project?
+// A: To split the videos based on uttered words and to convert them into frames we used movie.pyn We used keras tensorflow to build the model. Opencv and facial landmark is used to track the lip movement. We used geometric shape of lip to extract feature and lstm is used as language model.
+// ==========================================
+// Q: Why do you want to switch your current job?
+// A: I have thoroughly enjoyed working for my employer and I have achieved some great things whilst I have been there. I must say my colleagues and senior were very friendly and helpful there. They have guided me well as a fresher. There was a work life balance in my previous company. But suddenly because of some personal reasons, I have to shift my home. And now my office location has become far away from my home.
+// ==========================================
+// Q: Why do you want to switch your career path from Engineering to executive role?
+// A: Although I am from an Engineering background but apart from this I was always interested in sales and marketing. Actually I am not switching my career path rather I am contributing more to the society by adding one more career path.
+// ==========================================
+// Q: Why should we hire you even you are from different background?
+// A: I think it is a kind of art to be proficient in sales. According to me it is a must to have a good sales strategy for any company to standout in the marketplace and to grab the attention of the targeted customers. As I was a student of CSE department and most of your courses are based on different framework and Computer based technologies So I have well enough idea about the customers demands and expectations. Moreover few months ago I have completed an online based course. Though the course was about web development but being a student of an online based educational community I know how a well organised course structure can be beneficial for the both ends supplier and customer. Durning my varsity days I was involved in various extra curriculum activities. I was a general member of NSU CEC where I worked as a volunteer in one of their events. So I know better how to handle and communicate people with patience. I am quite interested in designing. I love to do designing using powerpoint. I think good and eye-catching designing in the advertisements  helps to accelerate the sales. In my company I have also create a presentation video on its 17th anniversary using powerpoint. I think my different background will not create any problem for the sale rather it will add an extra benefit to it.
+// ==========================================
+// Q: Why do you want to do part time job?
+// A: Part time work allows me the time to pursue the many interests I have including sales and software development.
+// ==========================================
+// Q: Why do you want more salary?
+// A: See, as how I see it since I have diverse skillset and have the ability to adopt different technologies and I have performed really well in my previous company and learned a lot of things. I I would continue to do that here as well  so I think It's an investment worth making.
+// ==========================================
+// Q:
+// A:
+// ==========================================
+// Q:
+// A:
+// ==========================================
+// Q:
+// A:
+// ==========================================
+// Q:
+// A:
+// ==========================================
+// Q:
+// A:
+// ==========================================
+// Q:
+// A:
+// ==========================================
+// Q:
+// A:
+// ==========================================
+// Q:
+// A:
+// ==========================================
+// Q:
+// A:
+// ==========================================
+// Q:
+// A:
+// ==========================================
+// Q:
+// A:
--- src/data/interviewPreparation/companyRelatedData.ts
+++ src/data/interviewPreparation/companyRelatedData.ts
@@ -5,3 +5,7 @@ export const companyRelatedData = [
     },
   ];
   
+  // ===
+
+// Question: 5. Why do you want to Join with Us?
+// Answer: I believe, your company is a great fit for my skills and experience. I appreciate your focus on employee development and growth. Moreover, job's hybrid nature will allow me flexible work location which is conducive to my productivity and comfort.
