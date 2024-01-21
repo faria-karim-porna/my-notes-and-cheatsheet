1469ccbcb122c43f92a3d90e3f97b799e6f9a5b1
faria-karim-porna <faria.porna.08@gmail.com>
2024-01-21 12:11:21 +0000

    Added Cheatsheet Design Patterns Page

---
+++ src/components/pages/cheatsheets/CheatsheetDesignPatterns.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Accordion } from "../../common/Accordion";
+import { CodeBlock } from "../../common/Codeblock";
+
+const CheatSheetDesignPatternsComponent = () => {
+  return (
+    <>
+      <div>Design Patterns Cheatsheet</div>
+      <hr />
+
+      <Accordion outer="MongoDB, Node.js And TypeScript (Node Version: 18.16.0)">
+        <Accordion outer="Necessary Setup And Installation">
+          <div>Create a folder in the preferred location of the pc</div>
+          <div>Named the folder as the project name</div>
+          <div>
+            Write the following command inside the terminal of the folder
+          </div>
+          <Accordion outer="Terminal Codes">
+            <CodeBlock language="bash">{`npm install express cors mongoose`}</CodeBlock>
+            <CodeBlock language="bash">{`npm install @types/node @types/express @types/mongoose @types/cors`}</CodeBlock>
+            <CodeBlock language="bash">{`npm install -D concurrently nodemon`}</CodeBlock>
+            <CodeBlock language="bash">{`npm install dotenv`}</CodeBlock>
+            <CodeBlock language="bash">{`npm install mongodb`}</CodeBlock>
+            <CodeBlock language="bash">{`npm install body-parser`}</CodeBlock>
+            <CodeBlock language="bash">{`npm install @types/body-parser`}</CodeBlock>
+          </Accordion>
+          <div>
+            Only for authentication, write the following command inside the
+            terminal of the folder
+          </div>
+          <Accordion outer="Terminal Codes">
+            <CodeBlock language="bash">{`npm install bcryptjs jsonwebtoken`}</CodeBlock>
+            <CodeBlock language="bash">{`npm install --save-dev @types/bcryptjs`}</CodeBlock>
+            <CodeBlock language="bash">{`npm install --save-dev @types/jsonwebtoken`}</CodeBlock>
+          </Accordion>
+        </Accordion>
+      </Accordion>
+    </>
+  );
+};
+
+export const CheatSheetDesignPatterns = React.memo(
+  CheatSheetDesignPatternsComponent
+);
